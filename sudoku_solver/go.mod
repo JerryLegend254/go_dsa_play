75f6d810d368ef014d8b3faaf18697e768d38263
JerryLegend254 <jerryokuto713@gmail.com>
2024-10-09 21:54:53 +0300

    feat: added test for isInvalid func(sudoku_solver)

---
+++ sudoku_solver/go.mod
@@ -0,0 +1,3 @@
+module github.com/JerryLegend254/go_dsa_play/sudoku_solver
+
+go 1.23.1
---
+++ sudoku_solver/main_test.go
@@ -0,0 +1,95 @@
+package main
+
+import "testing"
+
+// Test for the isValid function
+func TestIsValid(t *testing.T) {
+	tests := []struct {
+		title    string    // Title of the test case
+		board    [N][N]int // Sudoku board
+		row      int       // Row to place the number
+		col      int       // Column to place the number
+		num      int       // Number to place
+		expected bool      // Expected result (true if valid, false if not)
+	}{
+		{
+			title: "Valid placement",
+			board: [N][N]int{
+				{5, 3, 0, 0, 7, 0, 0, 0, 0},
+				{6, 0, 0, 1, 9, 5, 0, 0, 0},
+				{0, 9, 8, 0, 0, 0, 0, 6, 0},
+				{8, 0, 0, 0, 6, 0, 0, 0, 3},
+				{4, 0, 0, 8, 0, 3, 0, 0, 1},
+				{7, 0, 0, 0, 2, 0, 0, 0, 6},
+				{0, 6, 0, 0, 0, 0, 2, 8, 0},
+				{0, 0, 0, 4, 1, 9, 0, 0, 5},
+				{0, 0, 0, 0, 8, 0, 0, 7, 9},
+			},
+			row:      0,
+			col:      2,
+			num:      4,
+			expected: true,
+		},
+		{
+			title: "Invalid placement due to number in row",
+			board: [N][N]int{
+				{5, 3, 0, 0, 7, 0, 0, 0, 0},
+				{6, 0, 0, 1, 9, 5, 0, 0, 0},
+				{0, 9, 8, 0, 0, 0, 0, 6, 0},
+				{8, 0, 0, 0, 6, 0, 0, 0, 3},
+				{4, 0, 0, 8, 0, 3, 0, 0, 1},
+				{7, 0, 0, 0, 2, 0, 0, 0, 6},
+				{0, 6, 0, 0, 0, 0, 2, 8, 0},
+				{0, 0, 0, 4, 1, 9, 0, 0, 5},
+				{0, 0, 0, 0, 8, 0, 0, 7, 9},
+			},
+			row:      0,
+			col:      2,
+			num:      7,
+			expected: false,
+		},
+		{
+			title: "Invalid placement due to number in column",
+			board: [N][N]int{
+				{5, 3, 0, 0, 7, 0, 0, 0, 0},
+				{6, 0, 0, 1, 9, 5, 0, 0, 0},
+				{0, 9, 8, 0, 0, 0, 0, 6, 0},
+				{8, 0, 0, 0, 6, 0, 0, 0, 3},
+				{4, 0, 0, 8, 0, 3, 0, 0, 1},
+				{7, 0, 0, 0, 2, 0, 0, 0, 6},
+				{0, 6, 0, 0, 0, 0, 2, 8, 0},
+				{0, 0, 0, 4, 1, 9, 0, 0, 5},
+				{0, 0, 0, 0, 8, 0, 0, 7, 9},
+			},
+			row:      1,
+			col:      3,
+			num:      4,
+			expected: false,
+		},
+		{
+			title: "Invalid placement due to number in 3x3 subgrid",
+			board: [N][N]int{
+				{5, 3, 0, 0, 7, 0, 0, 0, 0},
+				{6, 0, 0, 1, 9, 5, 0, 0, 0},
+				{0, 9, 8, 0, 0, 0, 0, 6, 0},
+				{8, 0, 0, 0, 6, 0, 0, 0, 3},
+				{4, 0, 0, 8, 0, 3, 0, 0, 1},
+				{7, 0, 0, 0, 2, 0, 0, 0, 6},
+				{0, 6, 0, 0, 0, 0, 2, 8, 0},
+				{0, 0, 0, 4, 1, 9, 0, 0, 5},
+				{0, 0, 0, 0, 8, 0, 0, 7, 9},
+			},
+			row:      1,
+			col:      1,
+			num:      9,
+			expected: false,
+		},
+	}
+
+	for _, test := range tests {
+		result := isValid(&test.board, test.row, test.col, test.num)
+		if result != test.expected {
+			t.Errorf("%v case failed: expected %v, got %v", test.title, test.expected, result)
+		}
+	}
+}
